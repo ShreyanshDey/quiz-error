b7163b3b712fe6f393b4712510237db2702b940e
ShreyanshDey <82038005+ShreyanshDey@users.noreply.github.com>
2021-07-19 19:57:01 +0530

    Add files via upload

---
+++ Proc-37-template-main/Proc-37-template-main/js/Quiz.js
@@ -0,0 +1,65 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    this.title.hide();
+    this.input1.hide();
+    this.input2.hide();
+    this.button.hide();
+    //write code to change the background color here
+    
+    //write code to show a heading for showing the result of Quiz
+
+      Contestant.getPlayerInfo() 
+   
+
+    //write condition to check if contestantInfor is not undefined
+    if (allContestants !== undefined) {
+      fill("blue")
+      textSize(20)
+      text("Winner's name is highlighted in green")
+    
+    //write code to highlight contest who answered correctly
+    for (var plr in allContestants){
+        var correctAnswer="2";
+        if (correctAnswer === allContestants[plr].answer) 
+          fill ("green")
+        
+        else
+          fill("red")
+        
+        display_Answer+=30
+        textSize(20)
+        text(allContestants[plr].name+": "allContestants[plr].answer,250,display_Answer)
+}
+}
+}
+}
